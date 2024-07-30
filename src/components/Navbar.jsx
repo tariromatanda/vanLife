742d1a51ebeadf60735ccdcb4450df12807d3750
Kelvinnhengu <nhengukelvin@gmail.com>
2024-07-30 13:11:08 +0200

    kevy's commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <section className="nav-bar">
+      <div className="logo">
+        <h1>#CarRental</h1>
+      </div>
+      <nav className='navbar'>
+        <a href="">About</a>
+        <a href="">Van</a>
+      </nav>
+    </section>
+  );
+}
+
+export default Navbar
