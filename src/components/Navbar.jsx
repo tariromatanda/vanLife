7705c0ccee4d8cae9a7a278fb1553adf18a0526d
Dylan Maupa <dylanmaupa@gmail.com>
2024-07-31 08:09:21 +0200

    modified navBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,12 @@
-<<<<<<< HEAD
-import React from 'react'
-
-
-
-const Navbar = () => {
-  return (
-    <section className="nav-bar">
-      <div className="logo">
-        <h1>#CarRental</h1>
-      </div>
-      <nav className='navbar'>
-        <a href="">About</a>
-        <a href="">Van</a>
-      </nav>
-    </section>
-  )
-}
-
-export default Navbar
-=======
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Logo from './Logo'
 
 const Navbar = () => {
     return (
         <nav className='navBar'>
             <Link to='/'>
-                <h1 className="logo">#VANLIFE</h1>
+                <Logo logoTitle={'#VANLIFE'}/>
             </Link>
 
             <ul>
@@ -38,4 +18,3 @@ const Navbar = () => {
 }
 
 export default Navbar
->>>>>>> ca8968086e69521ff43f88fa25007f2868e254c2
