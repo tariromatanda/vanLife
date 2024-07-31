748dd508fbb202e1d5925b3dcf198fd45daaba1b
Kelvinnhengu <nhengukelvin@gmail.com>
2024-07-31 13:52:21 +0200

    #

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import vanfront from '../assets/vanfront.png'
 
 const About = () => {
   return (
-    <div>
-      
+    <div className='about'>
+      <div className="about-inner">
+        <img src={vanfront} alt="" />
+        
+      </div>
     </div>
   )
 }
