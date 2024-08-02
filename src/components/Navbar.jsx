dac78dad6cd7f732e882a03622682aab26a3331c
SaironzG <gsaironz@gmail.com>
2024-08-02 09:58:25 +0200

    change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-
-
-
-const Navbar = () => {
-  return (
-    <section className="nav-bar">
-      <div className="logo">
-        <h1>#CarRental</h1>
-      </div>
-      <nav className='navbar'>
-        <a href="">About</a>
-        <a href="">Van</a>
-      </nav>
-    </section>
-  )
-}
-
-export default Navbar
-=======
 import React from 'react'
 import { Link } from 'react-router-dom'
 
@@ -38,4 +17,3 @@ const Navbar = () => {
 }
 
 export default Navbar
->>>>>>> ca8968086e69521ff43f88fa25007f2868e254c2
